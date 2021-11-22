677a5528d9d067a40c41ed043daf14275053f079
Cole <cebCole@gmail.com>
2021-11-22 14:37:46 -0600

    main.js added in missing this.

--- resources/js/main.js
+++ resources/js/main.js
@@ -11,12 +11,12 @@ class Main {
 		this.myCanvasLeft = this.myCanvas.offsetLeft + this.myCanvas.clientLeft
 		this.myCanvasTop = this.myCanvas.offsetTop + this.myCanvas.clientTop
 		
-		this.canvasWidth = myCanvasCtx.canvas.clientWidth;
-		this.canvasHeight = myCanvasCtx.canvas.clientHeight;
+		this.canvasWidth = this.myCanvasCtx.canvas.clientWidth;
+		this.canvasHeight = this.myCanvasCtx.canvas.clientHeight;
 		this.cellWidth = 50;
 		
-		this.canvasWidthCellWidth = canvasWidth / cellWidth;
-		this.canvasHeightCellHeight = canvas.Height / cellWidth;
+		this.canvasWidthCellWidth = this.canvasWidth / this.cellWidth;
+		this.canvasHeightCellHeight = this.canvas.Height / this.cellWidth;
 		
 		this.isRunning = true;
 		this.delay = 300;	
@@ -27,15 +27,15 @@ class Main {
 		this.board.setCellNeighbours();
 		
 		this.myCanvas.addEventListener('click', function(event) {
-			var x = event.pageX - myCanvasLeft;
-			var y = event.pageY - myCanvasTop;
+			var x = event.pageX - this.myCanvasLeft;
+			var y = event.pageY - this.myCanvasTop;
 			
 			this.board.board.forEach(function(row) {
 				row.forEach(function(cell) {
 					if (y > cell.y*cell.width && y < cell.y*cell.width + cell.width
 					&& x > cell.x*cell.width && x < cell.x*cell.width + cell.width) {
 						cell.drawState();
-						this.myCanvasCtx.clearRect(0,0, myCanvas.width, myCanvas.height);
+						this.myCanvasCtx.clearRect(0,0, this.myCanvas.width, this.myCanvas.height);
 						this.board.boardUpdate();
 					}
 				})
