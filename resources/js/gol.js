5a26cf3133a7ef237ebcc6cc6ac4eb71e049e468
Cole <cebCole@gmail.com>
2021-12-03 05:53:03 -0600

    game of life module

---
+++ resources/js/gol.js
@@ -0,0 +1,60 @@
+import {Board} from './board.js'
+import * as cons from '.constanst.js'
+
+
+export class GameOfLife {
+	constructor () {
+		this.board = new Board(cons.BOARD_WIDTH, cons.BOARD_HEIGHT)
+		this.isRunning = false;
+		this.delay = cons.DELAY;	
+	}
+	
+	initializeBoard() {
+		this.board.createBoard();
+		this.board.setCellNeighbours();
+		
+		const gameBoard = this.board.board
+		
+		//Mouse Controls
+		CANVAS.addEventListener('click', (event) => {
+			const x = event.pageX - cons.CANVAS_LEFT;
+			const y = event.pageY - cons.CANVAS_TOP;
+			
+			gameBoard.forEach((row) => {
+				row.forEach((cell) => {
+					if (y > cell.y*cell.width && y < cell.y*cell.width + cell.width
+					&& x > cell.x*cell.width && x < cell.x*cell.width + cell.width) {
+						cell.drawState();
+						CTX.clearRect(0,0, cons.CANVAS_WIDTH, cons.CANVAS_HEIGHT);
+						this.Board.boardUpdate(CTX);
+					}
+				})
+			})
+		}, false);
+	    
+		this.board.setPrevState();
+		this.board.boardUpdate(cons.CTX);
+	}
+	
+	updateGame() {
+		cons.CTX.clearRect(0,0, cons.CANVAS_WIDTH, cons.CANVAS_HEIGHT);
+		this.board.nextState();
+		this.board.setPrevState();
+		this.board.boardUpdate(cons.CTX);
+	}
+	
+	
+	runGame() {
+		const self = this
+		
+		function mainLoop() {
+			if (self.isRunning) {
+				self.updateGame();
+				setTimeout( () => {
+					window.requestAnimationFrame(mainLoop);
+				}, self.delay);
+			}
+		}
+		window.requestAnimationFrame(mainLoop)
+	}
+}
