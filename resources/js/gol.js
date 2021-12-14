c182149c659f30afe35238730d04dc8ad9ff8da5
Cole <cebCole@gmail.com>
2021-12-14 07:03:44 -0600

    clog tests

--- resources/js/gol.js
+++ resources/js/gol.js
@@ -113,6 +113,7 @@ export class GameOfLife {
 							this.lastChange = cell
 							console.log(this.lastChange)
 							console.log(cell)
+							console.log(this.lastChange === cell)
 							
 							cell.drawState();
 							cons.CTX.clearRect(0,0, cons.CANVAS_WIDTH, cons.CANVAS_HEIGHT);
