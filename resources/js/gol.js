0ecb2aea41a5e1011690cf9535cd57a6f9f7a599
Cole <cebCole@gmail.com>
2021-12-21 06:16:25 -0600

    added missing this

--- resources/js/gol.js
+++ resources/js/gol.js
@@ -117,7 +117,7 @@ export class GameOfLife {
 		//Mouse Controls
 		this.mouseClick(gameBoard);
 		this.mouseMoveWhilstDown((event) => this.mouseMoveFunction(gameBoard))
-		cons.CANVAS.addEventListener("touchmove", touchMoveFunction, false)
+		cons.CANVAS.addEventListener("touchmove", this.touchMoveFunction, false)
 		this.board.setPrevState();
 		this.board.boardUpdate(cons.CTX);
 	}
