72b3a8fd2608d1a2a2afd12a20dad411438bb56c
Cole <cebCole@gmail.com>
2022-01-26 07:51:10 -0600

    changed default delay starting value

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -4,7 +4,7 @@ export const CANVAS_LEFT = CANVAS.offsetLeft + CANVAS.clientLeft
 export const CANVAS_TOP = CANVAS.offsetTop + CANVAS.clientTop
 export const CANVAS_WIDTH = CANVAS.width;
 export const CANVAS_HEIGHT = CANVAS.height;
-export const DELAY = 100;
+export const DELAY = 300;
 
 
 export const CELL_WIDTH = 25;
